5e10cc3396e5fd0175ea8098ba42a56ef3a80832
Tran Duc Tri <ductri179@gmail.com>
2018-09-25 08:41:52 +0700

    Add files via upload
    buoi 2

---
+++ Algorithms/src/com/oscartran/bigocoding/Array_224B.java
@@ -0,0 +1,44 @@
+
+package com.oscartran.codeforces.day2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Array_224B {
+    public static void main(String[] args) {
+        int max = (int)10e5 + 1;
+        int c[] = new int[max];
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n, k, count = 0;
+        n = sc.nextInt();
+        k = sc.nextInt();
+        
+        List<Integer> arr = new ArrayList<>();
+        arr.add(0);
+        
+        for (int i = 1; i <= n; i++) {
+            arr.add(sc.nextInt());
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            c[arr.get(i)]++;
+            if (c[arr.get(i)] == 1) {
+                count++;
+            }
+            int j = 0;
+            while (count == k ) {
+                j++;
+                c[arr.get(j)]--;
+                if (c[arr.get(j)] == 0) {
+                    System.out.println(j + " " + i);
+                    return;
+                }
+            }
+        }
+        System.out.println("-1 -1");
+        
+    }
+}
---
+++ Algorithms/src/com/oscartran/bigocoding/GeorgeandRound_387B.java
@@ -0,0 +1,48 @@
+
+package com.oscartran.codeforces.day2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GeorgeandRound_387B {
+    public static void main(String args[]) {
+        int max = (int) 10e6 + 1;
+        Scanner sc = new Scanner(System.in);
+       
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        List<Integer> a = new ArrayList<>();
+        int[] b = new int[max];
+        
+        for (int i=0; i<n; i++) {
+            a.add(sc.nextInt());
+        }
+        
+        for (int i=0; i<m; i++) {
+            b[sc.nextInt()]++;
+        }
+        
+        for (int i=0; i<a.size(); i++) {
+            if (b[a.get(i)] > 0) {
+                b[a.get(i)]--;
+                a.remove(i--);
+            }
+        }
+        
+        int j = a.size() > 0? a.get(0) : 0;
+        for (int i=0; i<a.size(); i++) {
+            for (; j < max; j++) {
+                if (b[j] >0) {
+                    b[j--]--;
+                    a.remove(i--);
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(a.size());
+    }
+}
